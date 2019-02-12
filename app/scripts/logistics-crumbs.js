dc4882618c769a3eaaeffef057a2739a7d34bbcf
Aleksei Arro <alexey.arro@waytostart.ru>
2019-02-12 19:18:04 +0300

    Test is ok

---
+++ app/scripts/logistics-crumbs.js
@@ -0,0 +1,22 @@
+/* eslint-disable */
+
+$(document).ready(function(){
+  $('.logistics-crumbs__list--europe').slick({
+    arrows: false,
+    infinite: false,
+    slidesToScroll: 2,
+    variableWidth: true,
+  });
+  $('.logistics-crumbs__list--asia').slick({
+    arrows: false,
+    infinite: false,
+    slidesToScroll: 2,
+    variableWidth: true,
+  });
+  $('.logistics-crumbs__list--america').slick({
+    arrows: false,
+    infinite: false,
+    slidesToScroll: 1,
+    variableWidth: true,
+  });
+});
