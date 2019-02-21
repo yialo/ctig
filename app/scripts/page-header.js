710e4674513abbe2fb6e24d91e8a5b7d8486c960
Aleksei Arro <polsck@gmail.com>
2019-02-21 22:29:19 +0300

    page-header js improved

--- app/scripts/page-header.js
+++ app/scripts/page-header.js
@@ -1,6 +1,7 @@
 /* eslint-disable */
 
 $(document).ready(function(){
+  var KEYCODE_ESC = 27;
   var page = $('.page');
   var navOpener = $('.navigation-toggle__button--opener');
   var navCloser = $('.navigation-toggle__button--closer');
@@ -8,6 +9,7 @@ $(document).ready(function(){
   var certsCloser = $('.certificates__return-button');
   var calcOpener = $('.calculator-toggle__button');
   var calcCloser = $('.modal__close-button');
+  var calcOverlay = $('.modal__overlay');
 
   navOpener.click(function(){
     page.addClass('is-nav-shown');
@@ -33,5 +35,25 @@ $(document).ready(function(){
   });
   calcCloser.click(function(){
     page.removeClass('is-modal-shown');
+  });
+  calcOverlay.click(function(){
+    if (page.hasClass('is-modal-shown')) {
+      page.removeClass('is-modal-shown');
+    }
+  });
+
+  $(window).keydown(function(evt){
+    if (evt.keyCode === KEYCODE_ESC) {
+      if (page.hasClass('is-modal-shown')) {
+        evt.preventDefault();
+        page.removeClass('is-modal-shown');
+      } else if (page.hasClass('is-certificates-shown')) {
+        evt.preventDefault();
+        page.removeClass('is-certificates-shown');
+      } else if (page.hasClass('is-nav-shown')) {
+        evt.preventDefault();
+        page.removeClass('is-nav-shown');
+      }
+    }
   });
 });
